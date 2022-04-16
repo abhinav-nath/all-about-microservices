d5203af227224f9c09c9a7b43f298d9af9da427d
Abhinav Nath <abhinavnath@ymail.com>
2022-04-16 15:34:03 +0530

    Create new projects for command and query

---
+++ cqrs-and-event-sourcing/code/bank-account-command/src/main/java/com/codecafe/microservices/cqrs/BankAccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.microservices.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountCommandApplication.class, args);
+	}
+
+}
