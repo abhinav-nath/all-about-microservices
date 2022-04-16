c460b6af04fa51b7f658d611574cd1ce29dc5d95
Abhinav Nath <abhinavnath@ymail.com>
2022-04-16 21:31:03 +0530

    Add gradle.properties

--- cqrs-and-event-sourcing/code/bank-account-command/src/test/java/com/codecafe/microservices/cqrs/BankAccountCommandApplicationTests.java
+++ cqrs-and-event-sourcing/code/bank-account-command/src/test/java/com/codecafe/microservices/cqrs/BankAccountCommandApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankAccountCommandApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
